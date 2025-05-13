b4985d79b7c8b47b8b5ea17ee6b471bcd137ef1d
omyadav-ml <om.v90821@gmail.com>
2025-05-13 16:00:00 +0530

    Add conditionals practice (May 13, 2025)

---
+++ Foundation Python/05.py
@@ -0,0 +1,108 @@
+# # # a=int(input("Enter Your Age: "))
+
+# # # if(a%2==0):
+# # #     print("The number is even.")
+
+# # # else:
+# # #     print("The number is odd.")
+# # # if a>=18:
+# # #     print("Yes you are eligible to vote.")
+    
+# # # elif a==0:
+# # #     print("You have not entered any age.")
+    
+# # # else:
+# # #     print("No You are not eligible to vote.")
+
+# # # print("End of the program.")
+
+
+# c=int(input("Enter Percentage "))
+    
+# if(c>=90):
+#     print("Excellent")
+# elif(c>=80):
+#     print("Very Good")
+# elif(c>=70):
+#     print("Good")
+# elif(c>=60):
+#     print("Average")     
+# elif(c>=50):
+#     print("Below Average")
+# else:
+#     print("Poor")
+# print("Your Grade is :",c)
+# m1=int(input("Enter Marks 1:"))
+# m2=int(input("Enter Marks 2:"))
+# m3=int(input("Enter Marks 3:"))
+
+# total_percentage=(100*(m1+m2+m3)/300)
+
+# if(total_percentage>=40 and m1>=33 and m2>=33 and m3>=33):
+#     print("you are passed",total_percentage)
+
+# else:
+#     print("you are failed,try next year",total_percentage)
+
+list=["om","aryan","Babbu"]
+name=input("Enter Your Name:")
+if (name in list):
+    print("Name contain from the give list")
+else:
+    print("The Name is not in list")
+ 
+
+
+# c = int(input("Enter Percentage: "))
+
+# if c < 0 or c > 100:
+#     print("Invalid percentage")
+# else:
+#     if c >= 90:
+#         grade = "Excellent"
+#     elif c >= 80:
+#         grade = "Very Good"
+#     elif c >= 70:
+#         grade = "Good"
+#     elif c >= 60:
+#         grade = "Average"
+#     elif c >= 50:
+#         grade = "Below Average"
+#     else:
+#         grade = "Poor"
+
+#     print("Your Performance:", grade)
+#     print("Your Grade is:", c, "%")
+
+
+
+# a=int(input("Enter Number: "))
+# if (a==0):
+#     print("The Number is Zero")
+# elif (a%2==0):
+#     print("The Number is Even")
+# # else:
+#     print("The NUmber is odd")
+
+# a=int(input("Enter first Number: "))
+# b=int(input("Enter second Number: "))
+# operator = input("Enter operation (+, -, *, /): ")
+
+# if(operator=='+'):
+#     print(a+b)
+# elif(operator=='-'):
+#     print(a-b)
+# elif(operator=='*'):
+#     print(a*b)
+# elif(operator=='/'):
+#     print(a/b)
+
+num=int(input("Enter a Integer: "))
+if(num>0):
+    print("The Number is positive")
+elif(num<0):
+    print("The Number is Negative")
+else:
+    print("The Number zero")
+
+    
