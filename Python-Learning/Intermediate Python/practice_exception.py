cc529dfa82e9b7598e87ebf53ddacf26461b07bb
omyadav-ml <om.v90821@gmail.com>
2025-11-08 17:03:17 +0530

    Done List comprehension

--- Python-Learning/Intermediate Python/practice_exception.py
+++ Python-Learning/Intermediate Python/practice_exception.py
@@ -111,3 +111,7 @@ table=[num*i for i in range(1,11)]
 
 with open("tables.txt","a") as f:
     f.write(f"Table of {num} :{str(table)} \n")
+
+square=[x**2 for x in range(1,21) if x % 2==0]
+print(square)
+
