76ecaca856bc264fbc3d4d56182911b8e933ae37
omyadav-ml <om.v90821@gmail.com>
2026-01-14 23:26:50 +0530

    Changes in project

--- Python-Learning/Projects/simple calculator.py
+++ Python-Learning/Projects/simple calculator.py
@@ -1,28 +1,31 @@
-def add(a,b):
+def addition(a,b):
     return a+b
-def sub(a,b):
+def subtraction(a,b):
     return a-b
-def mul(a,b):
+def multiply(a,b):
     return a*b
-def div(a,b):
+def division(a,b):
     if b==0:
         return "Error : cannot divide by zero "
     return a/b
+def power(a,b):
+    return a**b
 
 while True:
     print("1.Add")
     print("2.Sub")
     print("3.Mul")
     print("4.Div")
-    print("5.Exit")
+    print("5.Pow")
+    print("6.Exit")
 
-    choice=input("Choose an option (1 to 5): ")
+    choice=input("Choose an option (1 to 6): ")
 
-    if choice == "5":
+    if choice == "6":
         break
 
 
-    if choice in ["1","2","3","4"]:
+    if choice in ["1","2","3","4","5"]:
         try:
             num1=float(input("Enter a number: "))
             num2=float(input("Enter a number: "))
@@ -30,13 +33,15 @@ while True:
             print("Invalid input ! Enter number only")
             continue
         if choice == "1":
-            result= add(num1,num2)
+            result= addition(num1,num2)
         elif choice == "2":
-            result= sub(num1,num2)
+            result= subtraction(num1,num2)
         elif choice == "3":
-            result= mul(num1,num2)
+            result= multiply(num1,num2)
         elif choice == "4":
-            result= div(num1,num2)
+            result= division(num1,num2)
+        elif choice == "5":
+            result= power(num1,num2)
 
         print("Result",result)
 
